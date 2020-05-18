852ccb828eb05ebe78970bcb110f348ad5ffffc7
gempir <daniel.pasch.s@gmail.com>
2020-05-18 22:17:12 +0200

    split instructions by ;

--- compiler.cpp
+++ compiler.cpp
@@ -1,7 +1,21 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <vector>
  
+std::vector<std::string> split(const std::string &s, char delim)
+{
+    std::stringstream ss(s);
+    std::string item;
+    std::vector<std::string> elems;
+    while (std::getline(ss, item, delim))
+    {
+        elems.push_back(item);
+    }
+
+    return elems;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc <= 1) { 
@@ -16,7 +30,11 @@ int main(int argc, char *argv[])
     strStream << inFile.rdbuf();
     std::string str = strStream.str();
 
-    
-    std::cout << str << "\n";
+    auto instructions = split(str, ';');
+
+    for (auto it = instructions.begin(); it != instructions.end(); ++it) {
+        std::cout << *it;
+    }
+
     return 0;
 }
